9587182bf8e840bd8a533a8c29e28c5add9f932e
SisleyMore <sdrmy.20@gmail.com>
2019-11-12 17:05:11 -0500

    estilos de vista de colaboradores

--- src/view/home.js
+++ src/view/home.js
@@ -2,49 +2,41 @@ import { controllerExit, createPost } from '../controller.js';
 import { currentUser } from '../model/model-firebase.js';
 import { viewPosts } from './post.js';
 
-export const viewHome = (arrPost) => {
+export const viewHome = () => {
   const homeContainer = document.createElement('div');
   homeContainer.innerHTML = '';
   const homeTemplate = `  
   <header>
     <div class="logo-bars">
-      <label for="toggle"><i class="fa fa-bars" aria-hidden="true"></i></label>
-      <img src="../img/foods-kids.png" alt="nombre foods kids de la página web"/>
+    <label id="imagen-perfil" for="toggle"><i class="fa fa-bars" aria-hidden="true"></i></label>
+    <li><input type="search"></li>
+    <li><img src="../img/ring.png"/></li>
     </div>
     <input type="checkbox" class="hide" id="toggle">  
-    <nav class="navbar">  
-      <ul class="main-nav">
-        <li><a href="#/profile">${currentUser().displayName ? `${currentUser().displayName}` : `${currentUser().email}`}</a></li>
-        <li><a href="#/home" id="cerrar">Cerrar Sesión </a></li>
+    <nav class="colorPrincipal">  
+      <ul class="main-nav flex">
+      <li><a href="#/profile">Nombre</a></li>
+        <li><a href="">Notificaciones</a></li>
+        <li><a href="#/home" id="cerrar"></a></li>
       </ul>
     </nav> 
   </header>
   <main>
-    <div class="container-user">
-      <img class="color-img" src="../img/fruit_1.jpg" alt="frutas tropicales">
-      <div class="email-user">
-        <img class="img-perfil" src="${currentUser().photoURL ? `${currentUser().photoURL}` : '../img/user.png'}" alt="foto de perfil extraida del email, google o facebook del usuario"/>
-        <p class="select">${currentUser().email}</p>
-      </div>
-    </div>
     <div class="total">
       <div class="colunm-post">
         <textarea class="estilotextarea" name="comentarios" required  placeholder="¿Que quieres compartir?" id="comentario"></textarea>
         <div class= "options-post">
-          <label id="btn-file" >
-            <input type="file" name ="fichero" id="fichero" class="hide">
-            <i id="archivo" class="btn-img pointer fa fa-picture-o" aria-hidden="true"></i>
-          </label>
-          <select class="pointer select" id="post-privacy" >
-            <option value="public" id="public">Public</option>
-            <option value="private" id="private">Private</option>
-          </select>
-          <input type="submit" value="Compartir"class="btn-compartir pointer" id="compartir">
+          <input type="submit" value="POSTULAR"class="btn-compartir pointer" id="compartir">
         </div>
       </div>
       <div class="posts-content" id="posts-content"></div>
     </div>
-  </main>`;
+  </main>
+  <footer class="footer">
+  <div class="flex">
+      
+  </div>
+</footer>`;
   homeContainer.innerHTML = homeTemplate;
   homeContainer.classList.add('container-home');
 
@@ -53,8 +45,19 @@ export const viewHome = (arrPost) => {
   const totalView = homeContainer.querySelector('#posts-content');
 
   exit.addEventListener('click', controllerExit);
-  buttonCompartir.addEventListener('click', createPost);
-  arrPost.forEach(obj => totalView.appendChild(viewPosts(obj)));
+  // buttonCompartir.addEventListener('click', createPost);
+  // arrPost.forEach(obj => totalView.appendChild(viewPosts(obj)));
+
+  // const menuHamburguesa = homeContainer.querySelector('#imagen-perfil');
+  // menuHamburguesa.addEventListener('click', () => {
+  //   const menuPerfil = document.createElement('div');
+  //   const hamburguesaTemplate = `<ul>
+  //   <li><button>kfgkdngkfdj</button></li>
+  //   </ul>`
+  //   menuPerfil.innerHTML = hamburguesaTemplate;
+    
+  // })
 
   return homeContainer;
 };
+
