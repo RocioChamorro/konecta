cafff7a3e30fc18207943d7d62e5d8c75eb0896d
SisleyMore <sdrmy.20@gmail.com>
2019-11-13 12:41:11 -0500

    cambios

--- src/view/oportunidadrrhh.js
+++ src/view/oportunidadrrhh.js
@@ -1,4 +1,5 @@
-export const viewOportunidad = () => {
-    const oportunidadContainer = document.createElement('div');
-    oportunidadContainer.innerHTML = '';
-    const oportunidadTemplate = `  
+// export const viewOportunidad = () => {
+//     const oportunidadContainer = document.createElement('div');
+//     oportunidadContainer.innerHTML = '';
+//     const oportunidadTemplate = ` `; 
+// });
