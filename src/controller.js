b5cf1133c95fbb4b47d55519a41f10a1a9a54136
SisleyMore <sdrmy.20@gmail.com>
2019-11-13 02:28:25 -0500

    cambios

--- src/controller.js
+++ src/controller.js
@@ -57,22 +57,22 @@ export const controllerLogin = () => {
 
     console.log(result);
     console.log(result.user.emailVerified);
-    const modalContent = 'Recuerda :';
-    const modalParrafo = 'Actualizar tus datos para contactarnos contigo';
-    const modalFooter = document.getElementById('modalFooter');
-    modalFooter.classList.remove('hide');
-    const actualizarDatos = document.getElementById('actualizar_datos');
-    console.log(actualizarDatos);
-     actualizarDatos.addEventListener('click', alert('hola'));
-    modalMessage(modalContent, modalParrafo, '/img/documento.png');
+    // const modalContent = 'Recuerda :';
+    // const modalParrafo = 'Actualizar tus datos para contactarnos contigo';
+    // const modalFooter = document.getElementById('modalFooter');
+    // modalFooter.classList.remove('hide');
+    // const actualizarDatos = document.getElementById('actualizar_datos');
+    // console.log(actualizarDatos);
+    //  actualizarDatos.addEventListener('click', alert('hola'));
+    // modalMessage(modalContent, modalParrafo, '/img/documento.png');
 
-    // changeRoute('#/home');
+   changeRoute('#/home');
 
-    /*  if (result.user.emailVerified === false) {
+     if (result.user.emailVerified === false) {
        document.getElementById('error').innerHTML = 'No has verificado tu dirección de email';
      } else {
        changeRoute('#/home');
-     } */
+     } 
   }).catch((error) => {
     const errorMessage = error.message;
     if (dni1 === '' || password === '') {
